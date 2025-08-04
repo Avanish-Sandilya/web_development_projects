48be1352a7c22c495321f7f92ed76824a9d8e9c0
avanish_sandilya <bhanusandilya74@gmail.com>
2025-08-04 14:58:29 +0530

    adding footer component

--- MemoryCardGame/src/App.jsx
+++ MemoryCardGame/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer';
 
 function App() {
   const [score, setScore] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
    <div>
     <Header score={score} highScore={highScore}/>
+    <Footer/>
    </div>
   )
 }
---
+++ MemoryCardGame/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div class="footer">
+      <p>Get one point by clicking an image but dont click any image twice</p>
+    </div>
+  );
+}
