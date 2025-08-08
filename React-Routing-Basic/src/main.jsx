4e325fd51f282c21def35aee41e57f6675e747f6
avanish_sandilya <bhanusandilya74@gmail.com>
2025-08-08 23:29:23 +0530

    moving routes to separate file

--- React-Routing-Basic/src/main.jsx
+++ React-Routing-Basic/src/main.jsx
@@ -2,32 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-import Profile from "./Profile.jsx";
-import Cricket from "./Cricket.jsx";
-import Football from "./Football.jsx";
-import ErrorPage from "./ErrorPage.jsx";
+import { routes } from "./routes.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement:<ErrorPage/>
-  },
-  // {
-  //   path: "profile",
-  //   element: <Profile />,
-  //   children: [
-  //     { path: "Cricket", element: <Cricket /> },
-  //     { path: "Football", element: <Football /> },
-  //   ],
-  // },
 
-  {
-  path:"profile/:name",
-  element:<Profile/>
-  }
-]);
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
---
+++ React-Routing-Basic/src/routes.jsx
@@ -0,0 +1,27 @@
+  import App from "./App";
+  import Profile from "./Profile";
+  import ErrorPage from "./ErrorPage";
+
+  const routes=[
+  
+  {
+    path: "/",
+    element: <App />,
+    errorElement:<ErrorPage/>
+  },
+  // {
+  //   path: "profile",
+  //   element: <Profile />,
+  //   children: [
+  //     { path: "Cricket", element: <Cricket /> },
+  //     { path: "Football", element: <Football /> },
+  //   ],
+  // },
+
+  {
+  path:"profile/:name",
+  element:<Profile/>
+  }
+];
+
+export {routes};
