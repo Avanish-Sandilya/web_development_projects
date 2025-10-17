bce1fa3444cbcc1c2c6430fa3941ba09d9d2c040
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-17 18:25:34 +0530

    adding comments

--- complete-javascript-course-master/18-forkify/final/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/final/src/js/controller.js
@@ -105,6 +105,7 @@ const controlAddRecipe = async function (newRecipe) {
 
     // Render bookmark view
     bookmarksView.render(model.state.bookmarks);
+    
 
     // Change ID in URL
     window.history.pushState(null, '', `#${model.state.recipe.id}`);
