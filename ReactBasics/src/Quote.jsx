f6eba385ff6fc54174c3b8a7b35ccb0668c79513
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-18 18:55:43 +0530

    adding Quote component

---
+++ ReactBasics/src/Quote.jsx
@@ -0,0 +1,14 @@
+function Quote(){
+    const style = {
+        fontStyle:'italic'
+    }
+    return(
+        <>
+        <h2 style={style}>Hustle</h2>
+        <h2 style={style}>Loyalty</h2>
+        <h2 style={style}>Respect</h2>
+        </>
+    )
+}
+
+export default Quote;
--- ReactBasics/src/main.jsx
+++ ReactBasics/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './Greetings.jsx'
+import Quote from './Quote.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting/>
+    <Quote/>
   </StrictMode>,
 )
