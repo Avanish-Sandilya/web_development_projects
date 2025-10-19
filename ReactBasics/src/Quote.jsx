4257d571029a7aa9b6052128403a8ecb6064412b
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-19 12:23:24 +0530

    adding List in Quote component

--- ReactBasics/src/Quote.jsx
+++ ReactBasics/src/Quote.jsx
@@ -1,14 +1,18 @@
-function Quote(){
-    const style = {
-        fontStyle:'italic'
-    }
-    return(
-        <>
-        <h2 style={style}>Hustle</h2>
-        <h2 style={style}>Loyalty</h2>
-        <h2 style={style}>Respect</h2>
-        </>
-    )
+function Quote() {
+  const style = {
+    fontStyle: "italic",
+  };
+
+  const words = ["Hustle", "Loyalty", "Respect"];
+
+  const wordList = words.map((word) => <li key={word}>{word}</li>);
+
+  return (
+    <>
+      <h2 style={style}>John Cena</h2>
+      <ul style={style}>{wordList}</ul>
+    </>
+  );
 }
 
 export default Quote;
