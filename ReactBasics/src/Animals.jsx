d4fdc349743c2de534cd9af2908ce98b4c667931
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-19 18:06:02 +0530

    refactoring animal component

--- ReactBasics/src/Animals.jsx
+++ ReactBasics/src/Animals.jsx
@@ -1,23 +1,24 @@
-function ListItem(prop){
-    return prop.animal.startsWith("C")? <li>{prop.animal}</li>:null;
+function ListItem(prop) {
+  return prop.animal.startsWith("C") ? <li>{prop.animal}</li> : null;
 }
 
-function List(prop){
-    const animalList=prop.animals.map((animal)=><ListItem key={animal} animal={animal}/>);
+function List(prop) {
+  const animalList = prop.animals.map((animal) => (
+    <ListItem key={animal} animal={animal} />
+  ));
 
-    return(<ul>{animalList}</ul>);
+  return <ul>{animalList}</ul>;
 }
 
-function AnimalList(){
-    const animals = ["Cow","Dog","Horse","Cat"];
+function AnimalList() {
+  const animals = ["Cow", "Dog", "Horse", "Cat"];
 
-    return(
-        <>
-        <h1>Animals</h1>
-        <List animals={animals}/>
-        </>
-        
-    )
+  return (
+    <>
+      <h1>Animals</h1>
+      <List animals={animals} />
+    </>
+  );
 }
 
 export default AnimalList;
