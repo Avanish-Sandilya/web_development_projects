8472265aefde0a4bbdaea7fd08216dc6b9d89993
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-20 13:32:36 +0530

    refactoring multiple components for readability

--- ReactBasics/src/Greetings.jsx
+++ ReactBasics/src/Greetings.jsx
@@ -1,5 +1,5 @@
-function Greeting(){
-    return <h1>&quot;I am the game, and I will play it.&quot;</h1>
+function Greeting() {
+  return <h1>&quot;I am the game, and I will play it.&quot;</h1>;
 }
 
 export default Greeting;
--- ReactBasics/src/main.jsx
+++ ReactBasics/src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Greeting from './Greetings.jsx'
-import Quote from './Quote.jsx'
-import AnimalList from './Animals.jsx'
-import BirdList from './Birds.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Greeting from "./Greetings.jsx";
+import Quote from "./Quote.jsx";
+import AnimalList from "./Animals.jsx";
+import BirdList from "./Birds.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greeting/>
-    <Quote/>
-    <AnimalList/>
-    <BirdList/>
-  </StrictMode>,
-)
+    <Greeting />
+    <Quote />
+    <AnimalList />
+    <BirdList />
+  </StrictMode>
+);
