f94f95ff0796e6ffe41a0c5b284cd8d84ef7c036
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-21 23:24:10 +0530

    adding reformats

--- ReactBasics/src/Books.jsx
+++ ReactBasics/src/Books.jsx
@@ -1,31 +1,28 @@
-function Book(prop){
-    return <li>{prop.bookName}</li>
+function Book(prop) {
+  return <li>{prop.bookName}</li>;
 }
 
-function BookList(prop){
-    const bookList = prop.books.map((book)=>(<Book key={book.id} bookName={book.name}/>));
+function BookList(prop) {
+  const bookList = prop.books.map((book) => (
+    <Book key={book.id} bookName={book.name} />
+  ));
 
-    return(
-        <ul>
-            {bookList}
-        </ul>
-    )
+  return <ul>{bookList}</ul>;
 }
 
-function Books(){
-    const books= [
-        {name:"Effective Java",id:crypto.randomUUID()},
-        {name:"Clean Code",id:crypto.randomUUID()},
-        {name:"Let Us C",id:crypto.randomUUID()}
+function Books() {
+  const books = [
+    { name: "Effective Java", id: crypto.randomUUID() },
+    { name: "Clean Code", id: crypto.randomUUID() },
+    { name: "Let Us C", id: crypto.randomUUID() },
+  ];
 
-    ];
-
-    return(
-        <>
-        <h1>Books</h1>
-         <BookList books={books}/>
-        </>
-        )
+  return (
+    <>
+      <h1>Books</h1>
+      <BookList books={books} />
+    </>
+  );
 }
 
 export default Books;
