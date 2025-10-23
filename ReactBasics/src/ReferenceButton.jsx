75df97fceab34618c720704d5ba7b7e55589336e
avanish_sandilya <bhanusandilya74@gmail.com>
2025-10-23 18:44:17 +0530

    adding reformats

--- ReactBasics/src/ReferenceButton.jsx
+++ ReactBasics/src/ReferenceButton.jsx
@@ -1,26 +1,37 @@
-function Button({text,color,fontsize,handleclick}){
-    const buttonStyle={
-        color:color,
-        fontSize:fontsize+"px",
-    }
+function Button({ text, color, fontsize, handleclick }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontsize + "px",
+  };
 
-    return(
-        <button onClick={handleclick} style={buttonStyle}>{text}</button>
-    )
+  return (
+    <button onClick={handleclick} style={buttonStyle}>
+      {text}
+    </button>
+  );
 }
 
+function ReferenceButton() {
+  const handleButtonClick = (url) => {
+    window.location.href = url;
+  };
 
-function ReferenceButton(){
-    const handleButtonClick=(url)=>{
-        window.location.href=url;
-    }
-
-    return(
-        <>
-        <Button handleclick={()=>handleButtonClick('https://www.theodinproject.com')} text="Odin Project" color="White" fontsize={12} />
-        <Button handleclick={()=>handleButtonClick('https://www.google.com')} text="Odin Project" color="White" fontsize={12}/>
-            </>
-    )
+  return (
+    <>
+      <Button
+        handleclick={() => handleButtonClick("https://www.theodinproject.com")}
+        text="Odin Project"
+        color="White"
+        fontsize={12}
+      />
+      <Button
+        handleclick={() => handleButtonClick("https://www.google.com")}
+        text="Google"
+        color="White"
+        fontsize={12}
+      />
+    </>
+  );
 }
 
 export default ReferenceButton;
