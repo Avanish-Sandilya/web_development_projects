26a041ae86d69bb904da807b31318509abb5d3ac
avanish_sandilya <bhanusandilya74@gmail.com>
2026-01-25 20:42:05 +0530

    adding boilerplate

--- Full_Stack_Open/part1/src/App.jsx
+++ Full_Stack_Open/part1/src/App.jsx
@@ -1,7 +1,10 @@
+import dynamicValues from "./anotherComponent"
 const App = () => {
+  console.log('Hello from component')
   return (
     <div>
       <p>Hello world</p>
+      <p>{dynamicValues}</p>
     </div>
   )
 }
