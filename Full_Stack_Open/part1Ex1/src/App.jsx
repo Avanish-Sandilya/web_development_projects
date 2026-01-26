66a0559501c2665b78195f3a5a1082e457dd3987
avanish_sandilya <bhanusandilya74@gmail.com>
2026-01-26 16:17:25 +0530

    adding excersise solution

--- Full_Stack_Open/part1Ex1/src/App.jsx
+++ Full_Stack_Open/part1Ex1/src/App.jsx
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   );
 };
@@ -17,9 +17,9 @@ const Part=(props)=>{
 const Content=(props)=>{
   return(
     <>
-    <Part partNumber={props.part1} excersiseNumber={props.exercises1}/>
-    <Part partNumber={props.part2} excersiseNumber={props.exercises2}/>
-    <Part partNumber={props.part3} excersiseNumber={props.exercises3}/>
+    <Part partNumber={props.course.parts[0].name} excersiseNumber={props.course.parts[0].exercises}/>
+    <Part partNumber={props.course.parts[1].name} excersiseNumber={props.course.parts[1].exercises}/>
+    <Part partNumber={props.course.parts[2].name} excersiseNumber={props.course.parts[2].exercises}/>
     </>
   )
 }
@@ -27,25 +27,43 @@ const Content=(props)=>{
 const Footer=(props)=>{
   return(
     <>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
     </>
   )
 }
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course ={
+    name:"Half Stack application development",
+    parts:[
+
+    {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+
+
+  ]
+  } 
+
+  
+
+  
 
   return (
     <>
     <Header course={course}/>
-    <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-    <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    <Content course={course}/>
+    <Footer course={course}/>
     </>
   );
 };
