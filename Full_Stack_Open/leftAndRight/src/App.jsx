0148073e663eeedae742dc32efa16ea9c43ff9e6
avanish_sandilya <bhanusandilya74@gmail.com>
2026-01-28 18:31:06 +0530

    adding new component

--- Full_Stack_Open/leftAndRight/src/App.jsx
+++ Full_Stack_Open/leftAndRight/src/App.jsx
@@ -1,16 +1,59 @@
 import { useState } from "react";
 
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+    setTotal(left + right+1)
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+    setTotal(left + right+1)
+  };
 
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {right}
-      <button onClick={()=>{setLeft(0);setRight(0)}}>Reset</button>
+      <button
+        onClick={() => {
+          setLeft(0);
+          setRight(0);
+          setAll([]);
+          setTotal(0);
+        }}
+      >
+        Reset
+      </button>
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };
