ced6b775ca5536d6134b89e206acfc587264b84d
avanish_sandilya <bhanusandilya74@gmail.com>
2026-01-29 18:01:32 +0530

    adding anecdotes component

--- Full_Stack_Open/Anecdotes/src/App.jsx
+++ Full_Stack_Open/Anecdotes/src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+function randomNumber(length) {
+  return Math.floor(Math.random() * length)
+}
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const anecdotes = [
+    { quote: 'If it hurts, do it more often.', vote: 0 },
+    { quote: 'Adding manpower to a late software project makes it later!', vote: 0 },
+    { quote: 'The first 90 percent of the code accounts for the first 90 percent of the development time...', vote: 0 },
+    { quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', vote: 0 },
+    { quote: 'Premature optimization is the root of all evil.', vote: 0 },
+    { quote: 'Debugging is twice as hard as writing the code in the first place.', vote: 0 },
+    { quote: 'Programming without console.log is like a doctor refusing X-rays.', vote: 0 },
+    { quote: 'The only way to go fast, is to go well.', vote: 0 }
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const nextAnecdote = () => {
+    setSelected(randomNumber(anecdotes.length))
+  }
+
+  const voteAnecdote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  const maxVotes=Math.max(...votes);
+  const highestIndex = votes.indexOf(maxVotes);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{anecdotes[selected].quote}</p>
+      <p>has {votes[selected]} votes</p>
+
+      <button onClick={voteAnecdote}>Vote</button>
+      <button onClick={nextAnecdote}>Next</button>
+      <h2>Higest voted quote</h2>
+      <p>{anecdotes[highestIndex].quote}</p>
     </>
   )
 }
