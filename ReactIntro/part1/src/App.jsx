57f9057de5e5c3a60a0bfc76a6f04768227f1029
avanish_sandilya <bhanusandilya74@gmail.com>
2026-04-22 20:47:14 +0530

    basic components

--- ReactIntro/part1/src/App.jsx
+++ ReactIntro/part1/src/App.jsx
@@ -1,8 +1,26 @@
 const App = () => {
+
+  const now = new Date();
+  const a =10;
+  const b = 20;
+  console.log(now, a+b);
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
+      <p>
+        <Hello name='George' age='21'/>
+      </p>
     </div>
+  )
+}
+
+const Hello=(props)=>{
+  return(
+    <span>Hello {props.name}, you are {props.age} years old</span>
   )
 }
 
